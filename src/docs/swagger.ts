1bcaef908bc57426d3b54eb657550f107f7da1fe
Rydhlnst <66582308+Rydhlnst@users.noreply.github.com>
2025-02-04 11:39:04 +0700

    swagger.ts (Template) || tsconfig.ts add (npx tsc --init)

---
+++ src/docs/swagger.ts
@@ -0,0 +1,44 @@
+import swaggerAutogen from "swagger-autogen";
+
+
+const doc = {
+    info: {
+        version: "v0.0.1",
+        title: "API Documentation",
+        description: "API Documentation"
+    },
+    servers: [
+        {
+        url: "http://localhost:3000/api",
+        description: "Local Server"
+    },
+        {
+        url: "https://backend-acara-ruby.vercel.app/",
+        description: "Deploy Server"
+    }
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: "http",
+                scheme: "bearer",
+            },
+        },
+        schemas: {
+            LoginRequest: {
+                identifier: "rydhlnst@gmail.com",
+                password: "12341234"
+            }
+        }
+    }
+
+}
+
+
+const outputFile = "./swagger_output.json";
+
+// Akan membaca endpoint2nya
+const endPointFiles = ["../routes/api.ts"];
+
+
+swaggerAutogen({openapi: "3.0.0"})(outputFile, endPointFiles, doc)
